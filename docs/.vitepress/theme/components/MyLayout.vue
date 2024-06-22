116950f6816caba52d3d776268d84b3604a9b4cf
Nacho <kulujun@gmail.com>
2024-06-22 12:00:59 +0800

    fix custom layout bug

--- docs/.vitepress/theme/components/MyLayout.vue
+++ docs/.vitepress/theme/components/MyLayout.vue
@@ -28,7 +28,12 @@ function checkVersion(elements: Array<VersionLink>, text: string) {
   }
   return false;
 }
-function loadsVersion(frontmatter: { versions: { id: any; vanilla: any; loaders: any[]; others: any[]; }; }, posts: any[]) {
+function loadsVersion(
+  frontmatter: {
+    versions: { id: any; vanilla: any; loaders: any[]; others: any[] };
+  },
+  posts: any[]
+) {
   const { path } = useRoute();
   // console.log(path);
   const versions: Array<Version> = [];
@@ -43,44 +48,66 @@ function loadsVersion(frontmatter: { versions: { id: any; vanilla: any; loaders:
     };
     const others = {};
     if (frontmatter.versions.id) {
-      posts?.forEach((element: { frontmatter: { versions: { id: any; vanilla: string; loaders: any[]; others: any[]; }; }; url: string; }) => {
-        if (
-          element.frontmatter.versions &&
-          element.frontmatter.versions.id &&
-          element.frontmatter.versions.id === frontmatter.versions.id &&
-          path !== element.url // 不能是自己页面的
-        ) {
-          if (checkVersion(vanillas, element.frontmatter.versions.vanilla)) {
-            vanillas.push({
-              text: element.frontmatter.versions.vanilla,
-              loader: "vanilla",
-              link: element.url,
-            });
+      posts?.forEach(
+        (element: {
+          frontmatter: {
+            versions: {
+              id: any;
+              vanilla: string;
+              loaders: any[];
+              others: any[];
+            };
+          };
+          url: string;
+        }) => {
+          if (
+            element.frontmatter.versions &&
+            element.frontmatter.versions.id &&
+            element.frontmatter.versions.id === frontmatter.versions.id &&
+            path !== element.url // 不能是自己页面的
+          ) {
+            if (checkVersion(vanillas, element.frontmatter.versions.vanilla)) {
+              vanillas.push({
+                text: element.frontmatter.versions.vanilla,
+                loader: "vanilla",
+                link: element.url,
+              });
+            }
+            if (element.frontmatter.versions) {
+              if (element.frontmatter.versions.loaders) {
+                element.frontmatter.versions.loaders.forEach(
+                  (ele: { loader: string; text: string; link: any }) => {
+                    if (
+                      ele.loader &&
+                      ele.loader != "vanilla" &&
+                      checkVersion(loaders[ele.loader], ele.text)
+                    ) {
+                      ele.link = element.url;
+                      loaders[ele.loader].push(ele);
+                    }
+                  }
+                );
+              }
+              if (element.frontmatter.versions.others) {
+                element.frontmatter.versions.others.forEach(
+                  (ele: VersionLink) => {
+                    if (ele.id) {
+                      if (others[ele.id] === undefined) {
+                        // 不存在先创建
+                        others[ele.id] = [];
+                      }
+                      if (checkVersion(others[ele.id], ele.text ?? "")) {
+                        ele.link = element.url;
+                        others[ele.id].push(ele);
+                      }
+                    }
+                  }
+                );
+              }
+            }
           }
-          element.frontmatter.versions.loaders.forEach((ele: { loader: string; text: string; link: any; }) => {
-            if (
-              ele.loader &&
-              ele.loader != "vanilla" &&
-              checkVersion(loaders[ele.loader], ele.text)
-            ) {
-              ele.link = element.url;
-              loaders[ele.loader].push(ele);
-            }
-          });
-          element.frontmatter.versions.others.forEach((ele: VersionLink) => {
-            if (ele.id) {
-              if (others[ele.id] === undefined) {
-                // 不存在先创建
-                others[ele.id] = [];
-              }
-              if (checkVersion(others[ele.id], ele.text ?? '')) {
-                ele.link = element.url;
-                others[ele.id].push(ele);
-              }
-            }
-          });
         }
-      });
+      );
     }
     if (frontmatter.versions.vanilla) {
       versions.push({
@@ -93,18 +120,21 @@ function loadsVersion(frontmatter: { versions: { id: any; vanilla: any; loaders:
     }
     if (frontmatter.versions.loaders)
       frontmatter.versions.loaders.forEach((element: VersionLink) => {
-        versions.push({ current: element, others: loaders[element.loader ?? ''] });
+        versions.push({
+          current: element,
+          others: loaders[element.loader ?? ""],
+        });
       });
     if (frontmatter.versions.others)
       frontmatter.versions.others.forEach((element: VersionLink) => {
-        versions.push({ current: element, others: others[element.id?? ''] });
+        versions.push({ current: element, others: others[element.id ?? ""] });
       });
   }
   // console.log(versions);
   return versions;
 }
-function findWriter(writers: { [x: string]: any; }) {
-  function isWriter(element: { name: any; }, index: any, array: any) {
+function findWriter(writers: { [x: string]: any }) {
+  function isWriter(element: { name: any }, index: any, array: any) {
     if (writers) {
       for (var i in writers) {
         if (writers[i] == element.name) {
